58a257bf8322a4959f2719328f77110f043cc8f8
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2023-12-17 22:04:12 +0800

    perf: build时指定路径为/manager

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsConfigPath from 'vite-tsconfig-paths'
 import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/manager',
     plugins: [
         react(),
         tsConfigPath(),
