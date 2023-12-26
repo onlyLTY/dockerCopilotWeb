e09913508946826f313cd25c7393a3b6aafaea9d
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2023-12-26 18:37:29 +0800

    fix: manager后与#之间缺少/的问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsConfigPath from 'vite-tsconfig-paths'
 import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/manager',
+    base: '/manager/',
     plugins: [
         react(),
         tsConfigPath(),
